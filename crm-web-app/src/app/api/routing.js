73f1f2f2e4ab5944c5364b7cac538f630b1f77f5
Atem1735 <116035957+AmmarAhmed1735@users.noreply.github.com>
2024-09-16 16:05:48 -0400

    Update routing.js
    added API logic

--- crm-web-app/src/app/api/routing.js
+++ crm-web-app/src/app/api/routing.js
@@ -1,1 +1,9 @@
+import { NextResponse } from 'next/server';
+export async function GET() {
+  const customers = [
+    { id: 1, name: 'Nazeer Test 1' },
+    { id: 2, name: 'TBD' },
+  ];
+  return NextResponse.json(customers);
+}
 

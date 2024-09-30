788a1b529e2441c6e168990cb1d02ba3d8853025
Atem1735 <116035957+AmmarAhmed1735@users.noreply.github.com>
2024-09-29 21:25:44 -0400

    Create page.tsx

---
+++ src/app/customers/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Customers = () => { //customer page
+  return (
+    <div>
+      <nav>nav</nav>
+      <h1>This is the customers Page</h1> 
+    </div>
+  );
+};
+export default Customers;
